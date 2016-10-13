87959ab7856125b7771843e2c4cbfd590593f412
RummenacherK <Krummenacher@live.com>
2016-10-12 20:26:09 -0500

    1st console program setup

--- 01RockPaperScissors.cs
+++ 01RockPaperScissors.cs
@@ -1,19 +1,26 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Program
+namespace ConsoleApplication2
 {
-    public static void Main()
+    class Program
     {
-        Console.WriteLine("Enter hand 1:");
-        string hand1 = Console.ReadLine().ToLower();
-        Console.WriteLine("Enter hand 2:");
-        string hand2 = Console.ReadLine().ToLower();
-        Console.WriteLine(CompareHands(hand1, hand2));
-    }
-    
-    public static string CompareHands(string hand1, string hand2)
-    {
-        // Your code here
-        return hand1 + " " + hand2;
+        static void Main()
+        {
+            Console.WriteLine("Enter hand 1:");
+            string hand1 = Console.ReadLine().ToLower();
+            Console.WriteLine("Enter hand 2");
+            string hand2 = Console.ReadLine().ToLower();
+            Console.WriteLine(CompareHands(hand1, hand2));
+        }
+
+        public static string CompareHands ( string hand1, string hand2)
+        {
+            //Your code here
+            return hand1 + "" + hand2;
+        }
     }
 }
