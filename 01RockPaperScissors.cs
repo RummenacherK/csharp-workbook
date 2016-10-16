45dfca4b5150a058b6cf7086d6a1f5c7dbdddf54
RummenacherK <Krummenacher@live.com>
2016-10-16 16:53:19 -0500

    Homework 1 v1.1

--- 01RockPaperScissors.cs
+++ 01RockPaperScissors.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace RockPaperScissors
 {
     class Program
     {
@@ -12,45 +12,47 @@ namespace ConsoleApplication2
         {
             Console.WriteLine("Enter hand 1:");
             string hand1 = Console.ReadLine().ToLower();
-            Console.WriteLine("Enter hand 2");
+            Console.WriteLine("Enter hand 2:");
             string hand2 = Console.ReadLine().ToLower();
             Console.WriteLine(CompareHands(hand1, hand2));
         }
 
-        public static string CompareHands ( string hand1, string hand2)
+        public static string CompareHands(string hand1, string hand2)
         {
             //Your code here
             if (hand1 == hand2)
             {
-                Console.WriteLine("It's a tie!");
+                return "It's a tie!";
             }
-
             if (hand1 == "rock")
             {
                 if (hand2 == "scissors")
                 {
-                    Console.WriteLine("Hand one wins!");
+                    return "Hand one wins!";
                 }
-                Console.WriteLine("Hand two wins!");
+                return "Hand two wins!";
             }
 
             if (hand1 == "paper")
             {
                 if (hand2 == "rock")
                 {
-                    Console.WriteLine("Hand one wins!");
+                    return "Hand one wins!";
                 }
-                Console.WriteLine("Hand two wins!");
+                return "Hand two wins!";
             }
 
             if (hand1 == "scissors")
             {
                 if (hand2 == "paper")
                 {
-                    Console.WriteLine("Han one wins!");
+                    return "Hand one wins!";
                 }
-                Console.WriteLine("Hand two wins!");
+                return "Hand two wins!";
             }
+            return "Uh, that doesn't compute. Uh, wait, uh, you're under arrest!";
+            
         }
     }
 }
+
