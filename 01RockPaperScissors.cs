4fdaa7be35636eb482643c4c90ac2bfd78a97555
RummenacherK <Krummenacher@live.com>
2016-10-19 20:10:05 -0500

    RockPaperScissors classwork

--- 01RockPaperScissors.cs
+++ 01RockPaperScissors.cs
@@ -10,50 +10,54 @@ namespace RockPaperScissors
     {
         static void Main()
         {
-            Console.WriteLine("Enter hand 1:");
-            string hand1 = Console.ReadLine().ToLower();
-            Console.WriteLine("Enter hand 2:");
-            string hand2 = Console.ReadLine().ToLower();
-            Console.WriteLine(CompareHands(hand1, hand2));
+            Console.WriteLine("Play?(y/n)");
+            string playGame = Console.ReadLine().ToLower();
+            do
+            {
+                Console.WriteLine("Enter hand 1:");
+                string hand1 = Console.ReadLine().ToLower();
+                Console.WriteLine("Enter hand 2:");
+                string hand2 = Console.ReadLine().ToLower();
+                Console.WriteLine(CompareHands(hand1, hand2));
 
-            Console.ReadLine();
+                Console.ReadLine();
+            } while (playGame == "y");
         }
 
         public static string CompareHands(string hand1, string hand2)
         {
-            //Your code here
-            if (hand1 == hand2)
-            {
-                return "It's a tie!";
-            }
-            if (hand1 == "rock")
-            {
-                if (hand2 == "scissors")
+                if (hand1 == hand2)
                 {
-                    return "Hand one wins!";
+                    return "It's a tie!";
                 }
-                return "Hand two wins!";
-            }
+                if (hand1 == "rock")
+                {
+                    if (hand2 == "scissors")
+                    {
+                        return "Hand one wins!";
+                    }
+                    return "Hand two wins!";
+                }
 
-            if (hand1 == "paper")
-            {
-                if (hand2 == "rock")
+                if (hand1 == "paper")
                 {
-                    return "Hand one wins!";
+                    if (hand2 == "rock")
+                    {
+                        return "Hand one wins!";
+                    }
+                    return "Hand two wins!";
                 }
-                return "Hand two wins!";
-            }
 
-            if (hand1 == "scissors")
-            {
-                if (hand2 == "paper")
+                if (hand1 == "scissors")
                 {
-                    return "Hand one wins!";
+                    if (hand2 == "paper")
+                    {
+                        return "Hand one wins!";
+                    }
+                    return "Hand two wins!";
                 }
-                return "Hand two wins!";
-            }
-            return "Uh, that doesn't compute. Uh, wait, uh, you're under arrest!";
-            
+                return "Uh, that doesn't compute. Uh, wait, uh, you're under arrest!";
+          
         }
     }
 }
